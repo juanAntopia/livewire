87c3a95135956c41fdcdead1b77df681cfbe8dae
Juan Angel Hernández Antopia <42386043+juanAntopia@users.noreply.github.com>
2024-07-22 09:43:16 -0600

    feat: métodos mágicos

--- resources/views/livewire/paises.blade.php
+++ resources/views/livewire/paises.blade.php
@@ -1,18 +1,32 @@
 <div>
+
+    <div class="mb-5">
+        {{-- @livewire('hijo') --}}
+        <x-button wire:click="$set('count', 0)">resetear</x-button>
+    </div>
+
     <form wire:submit="save">
-        <x-input placeholder="Ingrese un país" wire:model="pais"></x-input>
+        <x-input placeholder="Ingrese un país" wire:model="pais" wire:keydown="increment"></x-input>
         <x-button>Agregar</x-button>
     </form>
 
-    <ul class="list-disc list-inside mt-5">
-        @foreach ($paises as $index => $pais)
+    <x-button wire:click="$toggle('openPaises')" class="mt-5">Mostrar/Ocultar</x-button>
+
+    @if ($openPaises)
+        <ul class="list-disc list-inside mt-5">
+            @foreach ($paises as $index => $pais)
             <li>
                 ({{ $index }}) <span wire:mouseenter="changeActive('{{ $pais }}')">{{ $pais }}</span>  <x-danger-button class="ml-5" wire:click="delete({{ $index }})">x</x-danger-button>
             </li>
-        @endforeach
-    </ul>
+            @endforeach
+        </ul>
+    @endif
 
     <div>
         {{ $active }}
+    </div>
+
+    <div>
+        {{ $count }}
     </div>
 </div>
