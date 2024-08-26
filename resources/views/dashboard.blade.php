2a7e0118b432eeb212facb3569cc85404197559d
Juan Angel Hernández Antopia <42386043+juanAntopia@users.noreply.github.com>
2024-08-26 10:09:09 -0600

    feat: formularios

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,7 +6,7 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 {{-- <x-welcome /> --}}
 
@@ -17,7 +17,9 @@
 
                 {{-- @livewire('contador') --}}
 
-                @livewire('paises')
+                {{-- @livewire('paises') --}}
+
+                @livewire('formulario')
 
             </div>
         </div>
