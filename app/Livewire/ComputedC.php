b7def1b34d4797368ca609cb98b523b6e3eecb93
Juan Angel Hernández Antopia <42386043+juanAntopia@users.noreply.github.com>
2025-03-05 17:18:43 -0600

    feat: propiedades computadas
    Ejemplo con propiedades computadas

---
+++ app/Livewire/ComputedC.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Post;
+use Livewire\Attributes\Computed;
+use Livewire\Component;
+
+class ComputedC extends Component
+{
+    public $post_id;
+
+    #[Computed]
+    public function post () {
+        return Post::find($this->post_id);
+    }
+
+    public function render()
+    {
+        return view('livewire.computed');
+    }
+}
---
+++ resources/views/livewire/computed.blade.php
@@ -0,0 +1,8 @@
+<div>
+    {{-- The best athlete wants his opponent at his best. --}}
+    <div class="mb-4">
+        <x-input class="w-full" type="number" wire:model.live="post_id"></x-input>
+    </div>
+
+    {{ $this->post }}
+</div>
